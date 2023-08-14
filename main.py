7e88d4e0cc8be6a426c089b677cc3ad34300dc92
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2023-08-14 10:22:26 +0100

    Feat: Initialize repository

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_links(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        links = []
+
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            if href:
+                links.append(href)
+
+        return links
+
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+        return []
+
+def write_to_txt(links):
+    with open("links.txt", "a") as f:
+        for link in links:
+            f.write(f"{link}\n")
+
+if __name__ == "__main__":
+    website_url = "https://www.foxnews.com/"
+    scraped_links = scrape_links(website_url)
+    write_to_txt(scraped_links)
+    
